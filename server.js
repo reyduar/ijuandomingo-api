4a084dfce48183887d7ba5d5de6e9636c1e06347
ariel.duarte <arielpy@gmail.com>
2018-02-17 02:09:14 -0300

    Set enviroment configuration to deploy

--- server.js
+++ server.js
@@ -3,16 +3,18 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 1245;
+var common = require('./routers/common')
+var config = common.config();
 
 var serverDev = 'mongodb://admin:secret@ds143737.mlab.com:43737/springdb';
 var serverProd = 'mongodb://admin:password@ds111851.mlab.com:11851/bdjdomprod';
 // MongoDb connection with mongoose
-mongoose.connect(serverDev, (err, res) => {
+mongoose.connect(config.mongodb_url, (err, res) => {
 	if(err){
-		console.error('Error to connect Database: bdjdom');
+		console.error('Error to connect Database de' + config.name);
 		throw err;
 	}else{
-		console.info('Se conectó con exito a la base de datos: bdjdom');
+		console.info('Se conectó con exito a la base de datos de '+ config.name);
 		app.listen(port, () => console.log(`Servidor ejecutandose en http://localhost:${port}`));
 	}
 });
